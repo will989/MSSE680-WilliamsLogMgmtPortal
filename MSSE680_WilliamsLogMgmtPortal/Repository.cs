c457f6b06fc0e1d50417fd426f97dcd735736f82
Andrew Williams <andrew_williams@comcast.net>
2013-09-12 19:23:19 -0600

    Repository class based on http://balmikighosh.blogspot.com/2012/03/creating-repository-pattern-with-entity.html

--- MSSE680_WilliamsLogMgmtPortal/Repository.cs
+++ MSSE680_WilliamsLogMgmtPortal/Repository.cs
@@ -9,13 +9,11 @@ using System.Configuration;
 
 namespace MSSE680_WilliamsLogMgmtPortal
 {
-    public class GenericRepository<T> : IDataRepository<T>, IDataRepository where T : class
-    {
-    }
-}
-
-
-
+/// <summary>
+    /// This is the Data Repository class
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class DataRepository<T> : IDataRepository<T>, IDataRepository where T : class
     {
         /// <summary>
         /// Data Context object to interact with the db
@@ -25,17 +23,173 @@ namespace MSSE680_WilliamsLogMgmtPortal
         /// <summary>
         /// Public constructor
         /// </summary>
-        public GenericRepository()
-       {
-            //get database connection
-            andy680Entities db = new andy680Entities();
- }
+        public DataRepository()
+        {
+            //instantiate the datacontext by reading the connection string
+            _dataContext = new DbContext(ConfigurationManager.ConnectionStrings["andy680entities"].ConnectionString);
+           
+        }
+        /// <summary>
+        /// Dispose method for the class
+        /// </summary>
+        public void Dispose()
+        {
+            if (_dataContext != null)
+            {
+                _dataContext.Dispose();               
+            }          
+        }
 
+        /// <summary>
+        /// This method is used to return a collection of objects
+        /// by specific key i.e a column name and the
+        /// specific value associated with the column
+        /// </summary>
+        /// <param name="KeyName">The name of the key</param>
+        /// <param name="KeyVal">The integer value of the column</param>
+        /// <returns></returns>
+        public virtual IQueryable<T> GetBySpecificKey(string KeyName, int KeyVal)
+        {
+
+            var itemParameter = Expression.Parameter(typeof(T), "item");
+            var whereExpression = Expression.Lambda<Func<T, bool>>
+                (
+                Expression.Equal(
+                    Expression.Property(
+                        itemParameter,
+                       KeyName
+                        ),
+                    Expression.Constant(KeyVal)
+                    ),
+                new[] { itemParameter }
+                );
+            try
+            {
+                return GetAll().Where(whereExpression).AsQueryable();
+            }
+            catch
+            {
+                return null;
+            }
+
+        }
+        /// <summary>
+        /// This method is used to return a collection of objects
+        /// by specific key i.e a column name and the
+        /// specific value associated with the column
+        /// </summary>
+        /// <param name="KeyName">The name of the key</param>
+        /// <param name="KeyVal">The string value of the column</param>
+        /// <returns></returns>
+        public virtual IQueryable<T> GetBySpecificKey(string KeyName, string KeyVal)
+        {
+
+            var itemParameter = Expression.Parameter(typeof(T), "item");
+            var whereExpression = Expression.Lambda<Func<T, bool>>
+                (
+                Expression.Equal(
+                    Expression.Property(
+                        itemParameter,
+                       KeyName
+                        ),
+                    Expression.Constant(KeyVal)
+                    ),
+                new[] { itemParameter }
+                );
+            try
+            {
+                return GetAll().Where(whereExpression).AsQueryable();
+            }
+            catch
+            {
+                return null;
+            }
+
+        }
+
+        /// <summary>
+        /// Returns all the records from a table
+        /// </summary>
+        /// <returns>Collection of records</returns>
+        public virtual IQueryable<T> GetAll()
+        {
+            return _dataContext.Set<T>().AsQueryable();            
+        }
+
+        /// <summary>
+        /// Inserts a record into the database
+        /// </summary>
+        /// <param name="entity">The entity to be inserted</param>
+        public virtual void Insert(T entity)
+        {
+            _dataContext.Set<T>().Add(entity);
+            _dataContext.SaveChanges();         
+
+        }
+
+        /// <summary>
+        /// Deletes a record from the table
+        /// </summary>
+        /// <param name="entity">Entity to be deleted</param>
+        public virtual void Delete(T entity)
+        {
+            var entry = _dataContext.Entry(entity);
+            if (entry != null)
+            {
+                entry.State = System.Data.EntityState.Deleted;
+            }
+            else
+            {
+                _dataContext.Set<T>().Attach(entity);
+            }
+            _dataContext.Entry(entity).State = System.Data.EntityState.Deleted;
+            _dataContext.SaveChanges();
+           
+        }
+        /// <summary>
+        /// Updates a record into a table
+        /// </summary>
+        /// <param name="entity"></param>
+        public virtual void Update(T entity)
+        {
+            _dataContext.Set<T>().Attach(entity);
+            _dataContext.Entry(entity).State = System.Data.EntityState.Modified;
+            _dataContext.SaveChanges();
+        }
+       
+        IQueryable IDataRepository.GetAll()
+        {
+            return GetAll();
+        }
+        void IDataRepository.Insert(object entity)
+        {
+            Insert((T)entity);
+        }
+        void IDataRepository.Update(object entity)
+        {
+            Update((T)entity);
+        }
+        void IDataRepository.Delete(object entity)
+        {
+            Delete((T)entity);
+        }      
+      
+        IQueryable IDataRepository.GetBySpecificKey(string KeyName, string KeyVal)
+        {
+            return GetBySpecificKey(KeyName, KeyVal);
+        }
+
+        IQueryable IDataRepository.GetBySpecificKey(string KeyName, int KeyVal)
+        {
+            return GetBySpecificKey(KeyName, KeyVal);
+        }
+    }
 
     /// <summary>
     /// Generic interface
     /// </summary>
-     public interface IDataRepository<T> where T : class
+    /// <typeparam name="T">Type of entity</typeparam>
+    public interface IDataRepository<T> where T : class
     {      
         IQueryable<T> GetAll();
         void Insert(T entity);
@@ -44,7 +198,6 @@ namespace MSSE680_WilliamsLogMgmtPortal
         IQueryable<T> GetBySpecificKey(string KeyName, string KeyVal);
         IQueryable<T> GetBySpecificKey(string KeyName, int KeyVal);
     }
-
     /// <summary>
     /// Generic interface
     /// </summary>
@@ -57,3 +210,4 @@ namespace MSSE680_WilliamsLogMgmtPortal
         IQueryable GetBySpecificKey(string KeyName, string KeyVal);
         IQueryable GetBySpecificKey(string KeyName, int KeyVal);
     }
+}
