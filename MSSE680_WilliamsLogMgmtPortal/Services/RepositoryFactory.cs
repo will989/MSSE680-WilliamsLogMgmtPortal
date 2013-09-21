9bbadc8ba5433b34b8b0a491b39ae7321bd77933
Andrew Williams <andrew_williams@comcast.net>
2013-09-20 21:55:33 -0600

    add factory and unit tests

---
+++ MSSE680_WilliamsLogMgmtPortal/Services/RepositoryFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MSSE680_WilliamsLogMgmtPortal.DAL;
+
+namespace MSSE680_WilliamsLogMgmtPortal.Services
+{
+    public class RepositoryFactory
+    {
+        //the create method takes a string that contains 
+        //a repository type as a parameter
+        public static IDataRepository Create(string sRepositoryType)
+        {
+            IDataRepository objRepo;
+            switch (sRepositoryType)
+            {
+                case "Organization":
+                    objRepo = new DataRepository<Organization>();
+                    break;
+                case "Message":
+                    objRepo = new DataRepository<Message>();
+                    break;
+                case "User":
+                    objRepo = new DataRepository<User>();
+                    break;
+                default:
+                    objRepo = null;
+                    throw new System.ArgumentException("Unimplemented Repository type the factory " + sRepositoryType);
+            }
+            return objRepo;
+        }
+    }
+}
---
+++ ServicesUnitTest/RepositoryFactoryTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSSE680_WilliamsLogMgmtPortal.DAL;
+
+namespace MSSE680_WilliamsLogMgmtPortal.ServicesUnitTest
+{
+    [TestClass]
+    public class RepositoryFactoryTests
+    {
+        //inserts an organization into the database using the repository
+        [TestMethod()]
+        public void InsertOrganizationUsingRepositoryFactory()
+        {
+
+            Organization organization = new Organization();
+            //organization.OrganizationId = 1;  - this is auto-assigned in db
+            organization.Name = "Repository Organization";
+            organization.Street = "Testing St";
+            organization.City = "Anytown";
+            organization.State = "CO";
+            organization.Zip = "80601";
+            organization.StartDate = System.DateTime.Now;
+
+            //use repository factory to create a repository
+            //of the correct type
+            var organizationRepository = RepositoryFactory.Create("Organization");
+
+            //add org to database
+            organizationRepository.Insert(organization);
+
+        }
+    }
+}
