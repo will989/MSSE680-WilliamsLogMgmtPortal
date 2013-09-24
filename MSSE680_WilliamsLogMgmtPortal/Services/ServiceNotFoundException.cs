63823e65e82e96906f952f0e7d45cad7869e410e
Andrew Williams <andrew_williams@comcast.net>
2013-09-23 19:06:57 -0600

    Adding exception class and testing github

---
+++ MSSE680_WilliamsLogMgmtPortal/Services/ServiceNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSSE680_WilliamsLogMgmtPortal.Services
+{
+    public class ServiceNotFoundException : Exception
+    {
+        public ServiceNotFoundException(String s) : base(s)
+        {
+
+        }
+    }
+}
